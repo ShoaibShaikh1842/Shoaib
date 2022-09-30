e8432441c3ede3b9fdd035cb384af1cde6a74dfb
ShoaibShaikh1842 <114721625+ShoaibShaikh1842@users.noreply.github.com>
2022-09-30 11:26:14 +0530

    Create file.cpp

---
+++ CPP/filehandling/file.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<fstream>
+using namespaces std;
+int main()
+{
+    char ch;
+    fstream fp;
+    fp=fopen("a.txt","ios::in");
+    while(ch!=-1)
+        
+    {
+        fp>>ch;
+        
+    }
+    
+}
