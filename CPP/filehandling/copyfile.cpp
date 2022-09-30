6824e8428713909a006fb28d83d068511652a5df
ShoaibShaikh1842 <114721625+ShoaibShaikh1842@users.noreply.github.com>
2022-09-30 12:02:13 +0530

    Create copyfile.cpp

---
+++ CPP/filehandling/copyfile.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+    fstream fp1,fp2;
+    char ch;
+    int cnt=0;
+
+    fp1.open("a.txt",ios::in);
+    fp2.open("b.txt",ios::out);
+    while(!fp1.eof())
+
+    {
+        fp1>>ch;
+        fp2<<ch;
+       }
+    fp1.close();
+      fp2.close();
+    return 0;
+}
